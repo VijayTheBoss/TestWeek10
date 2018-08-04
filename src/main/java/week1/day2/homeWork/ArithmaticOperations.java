afa0cdcaaa5cfab554eb6314813fdbe341220494
VijayTheBoss <vijaynataraj.kannan2014@gmail.com>
2018-08-04 17:57:58 +0530

    Base code

---
+++ src/main/java/week1/day2/homeWork/ArithmaticOperations.java
@@ -0,0 +1,15 @@
+package week1.day2.homeWork;
+
+public class ArithmaticOperations {
+	public static void main(String[] args) {
+		int number1=101;
+		int number2=4;
+		
+		System.out.println("Addition of two numbers is " + (number1+number2));
+		System.out.println("Sbraction of two numbers is " + (number1-number2));
+		System.out.println("Multiplication of two numbers is " + (number1*number2));
+		System.out.println("Division of two numbers is " + (number1/number2));
+		System.out.println("Modulas of two numbers is " + (number1%number2));
+	}
+
+}
